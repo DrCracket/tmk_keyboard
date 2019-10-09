ab5779e29f78e6e6d672604b1fd3a9b242549d52
Jonas Belouadi <bigjonas96@gmail.com>
2019-10-10 01:10:35 +0200

    Changed microcrontroller board to pro micro for adb

--- converter/adb_usb/flash.sh
+++ converter/adb_usb/flash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-make -f Makefile.rev1 KEYMAP=custom teensy 
+make -f Makefile.rev1 KEYMAP=custom
+sudo avrdude -p atmega32u4 -P /dev/ttyACM0  -c avr109  -U flash:w:adb_usb_rev1.hex
 make -f Makefile.rev1 clean
