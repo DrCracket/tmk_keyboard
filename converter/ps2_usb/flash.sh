8898b0e1cb48e61f7bb12abecc351227db5872f4
Jonas Belouadi <bigjonas96@gmail.com>
2020-03-24 00:26:17 +0100

    Added unimap for modified ibm model f at

--- converter/ps2_usb/flash.sh
+++ converter/ps2_usb/flash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make -f Makefile.32u4 KEYMAP=plain 
+make -f Makefile.32u4 KEYMAP=plain #at_custom
 sudo avrdude -p atmega32u4 -P /dev/ttyACM0  -c avr109  -U flash:w:ps2_usb_32u4.hex
 make -f Makefile.32u4 clean
---
+++ converter/ps2_usb/unimap_at_custom.c
@@ -0,0 +1,35 @@
+#include "unimap_trans.h"
+#include "action_layer.h"
+
+#define AC_FN0 ACTION_MACRO(0)
+
+#ifdef KEYMAP_SECTION_ENABLE
+const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
+#else
+const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
+#endif
+    UNIMAP_AT_CUSTOM(
+    F1,  F2,     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV,    FN0, PSCR,SLCK,PAUS, \
+    F3,  F4,     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSPC,   P7,  P8,  P9,  F11,  \
+    F5,  F6,     LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,    P4,  P5,  P6,  NO,   \
+    F7,  F8,     LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT ,SLSH,          RSFT,   P1,  P2,  P3,  F12,  \
+    F9,  PAUS,   CAPS,LALT,               SPC,                                    RALT,RCTL,        P0,  PDOT       \
+    ),
+    UNIMAP_AT_CUSTOM(
+    TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,PSLS,PAST,PMNS, \
+    TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,   TRNS,TRNS,TRNS,PPLS, \
+    TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   TRNS,TRNS,TRNS,TRNS, \
+    TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,          TRNS,   TRNS,TRNS,TRNS,PENT, \
+    TRNS,TRNS,   TRNS,TRNS,               TRNS,                                   TRNS,TRNS,        TRNS,TRNS       \
+    ),
+};
+
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        layer_invert(1);
+        return MACRO( T(NLCK), END );
+    }
+
+    return MACRO( END );
+}
