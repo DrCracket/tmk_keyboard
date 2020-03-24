dba2ba960d8c0b68dc72a9169eee8376e07a65af
Jonas Belouadi <bigjonas96@gmail.com>
2020-03-24 01:08:12 +0100

    Fixed wrong keycode in model f at keymap

--- converter/ps2_usb/unimap_at_custom.c
+++ converter/ps2_usb/unimap_at_custom.c
@@ -13,7 +13,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     F3,  F4,     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSPC,   P7,  P8,  P9,  F11,  \
     F5,  F6,     LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,    P4,  P5,  P6,  NO,   \
     F7,  F8,     LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT ,SLSH,          RSFT,   P1,  P2,  P3,  F12,  \
-    F9,  PAUS,   CAPS,LALT,               SPC,                                    RALT,RCTL,        P0,  PDOT       \
+    F9,  F10,    CAPS,LALT,               SPC,                                    RALT,RCTL,        P0,  PDOT       \
     ),
     UNIMAP_AT_CUSTOM(
     TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,PSLS,PAST,PMNS, \
