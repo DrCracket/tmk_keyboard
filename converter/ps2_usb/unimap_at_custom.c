eec6d210ab143106432e5bd79489f22b4c5a09fd
Jonas Belouadi <bigjonas96@gmail.com>
2020-03-26 14:59:02 +0100

    Caps key emits caps event on tap and super event on hold

--- converter/ps2_usb/unimap_at_custom.c
+++ converter/ps2_usb/unimap_at_custom.c
@@ -2,6 +2,7 @@
 #include "action_layer.h"
 
 #define AC_FN0 ACTION_MACRO(0)
+#define AC_FN1 ACTION_MODS_TAP_KEY(MOD_LGUI, KC_CAPS)
 
 #ifdef KEYMAP_SECTION_ENABLE
 const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section (".keymap.keymaps"))) = {
@@ -13,7 +14,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
     F3,  F4,     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,     BSPC,   P7,  P8,  P9,  F11,  \
     F5,  F6,     LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,          ENT,    P4,  P5,  P6,  NO,   \
     F7,  F8,     LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT ,SLSH,          RSFT,   P1,  P2,  P3,  F12,  \
-    F9,  F10,    CAPS,LALT,               SPC,                                    RALT,RCTL,        P0,  PDOT       \
+    F9,  F10,    FN1, LALT,               SPC,                                    RALT,RCTL,        P0,  PDOT       \
     ),
     UNIMAP_AT_CUSTOM(
     TRNS,TRNS,   TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   TRNS,PSLS,PAST,PMNS, \
